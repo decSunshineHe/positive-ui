de476b6b74822b741c2810da0a53b54bf89fa0be
何文杰 <hewenjie@hewenjiedeMacBook-Pro.local>
2023-03-21 17:54:55 +0800

    init project

---
+++ packages/components/src/SwitchStaff/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const SwitchStaff: FC = () => {
+  return (
+    <div>
+      <h1>SwitchStaff</h1>
+    </div>
+  );
+};
+
+export default SwitchStaff;
---
+++ packages/components/vite.config.ts
@@ -0,0 +1,91 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import decamelize from "decamelize";
+import { buildPlugin } from "vite-plugin-build";
+
+export const GLOBALS = {
+  react: "React",
+  "react-dom": "ReactDOM",
+  antd: "Antd",
+  classnames: "Classnames",
+  recharts: "Recharts",
+  "recharts-scale": "RechartsScale",
+  "business-utils": "BusinessUtils",
+};
+// 处理类库使用到的外部依赖
+// 确保外部化处理那些你不想打包进库的依赖
+export const EXTERNAL = [
+  "react",
+  "react-dom",
+  "antd",
+  "classnames",
+  "recharts",
+  "recharts-scale",
+  "business-utils",
+];
+
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react(),
+      buildPlugin({
+        libBuild: {
+          buildOptions: {
+            rollupOptions: {
+              external: EXTERNAL,
+              output: { globals: GLOBALS },
+            },
+            lib: {
+              entry: path.resolve(__dirname, "src/index.ts"),
+              name: "MaxtropyUI",
+              fileName: (format) => `maxtropy-ui.${format}.js`,
+            },
+          },
+        },
+      }),
+    ],
+    css: {
+      modules: {
+        localsConvention: "camelCaseOnly",
+        generateScopedName: (name: string, filename: string) => {
+          const match = filename
+            .replace(/\\/, "/")
+            .match(/.*\/src\/(.*)\/(.*)\.module\..*/);
+
+          if (match) {
+            let prefixName = match[1].replace(/\//g, "-");
+            if (match[2] !== "index") {
+              prefixName += `-${match[2]}`;
+            }
+            return `rabc-${decamelize(prefixName, {
+              separator: "-",
+            })}__${name}`;
+          }
+
+          return `rabc-${name}`;
+        },
+      },
+      postcss: {},
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+        },
+      },
+    },
+    resolve: {
+      alias: [
+        // fix less import by: @import ~
+        // less import no support webpack alias '~' · Issue #2185 · vitejs/vite
+        { find: /^~/, replacement: "" },
+      ],
+    },
+    test: {
+      environment: "happy-dom",
+      watch: false,
+    },
+  };
+});
