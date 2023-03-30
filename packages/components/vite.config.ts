d56be1a505203856dbf098061ad5040389ead157
何文杰 <hewenjie@hewenjiedeMacBook-Pro.local>
2023-03-30 10:49:53 +0800

    feat:css inject js

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { buildPlugin } from "vite-plugin-build";
 import path from "path";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
+      cssInjectedByJsPlugin(),
       buildPlugin({
         fileBuild: { emitDeclaration: true },
         libBuild: {
