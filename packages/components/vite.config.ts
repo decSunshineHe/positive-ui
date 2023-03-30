edae2185539948d5eced3470bdfe9453b4cf3439
何文杰 <hewenjie@hewenjiedeMacBook-Pro.local>
2023-03-30 11:41:35 +0800

    feat:样式加前缀

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { buildPlugin } from "vite-plugin-build";
 import path from "path";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import decamelize from "decamelize";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -27,5 +28,33 @@ export default defineConfig(({ mode }) => {
         },
       }),
     ],
+    css: {
+      modules: {
+        localsConvention: "camelCaseOnly",
+        generateScopedName: (name: string, filename: string) => {
+          const match = filename
+            .replace(/\\/, "/")
+            .match(/.*\/src\/(.*)\/(.*)\.module\..*/);
+
+          if (match) {
+            let prefixName = match[1].replace(/\//g, "-");
+            if (match[2] !== "index") {
+              prefixName += `-${match[2]}`;
+            }
+            return `positive-${decamelize(prefixName, {
+              separator: "-",
+            })}__${name}`;
+          }
+
+          return `positive-${name}`;
+        },
+        postcss: {},
+        preprocessorOptions: {
+          less: {
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
   };
 });
