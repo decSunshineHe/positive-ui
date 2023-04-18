6fd363940b61ac54da0fbdda378b99bfacc30ebb
decSunshineHe <1136031918@qq.com>
2023-04-18 16:30:50 +0800

    feat: 更新请求

--- packages/utils/src/api/staff.ts
+++ packages/utils/src/api/staff.ts
@@ -1,5 +1,5 @@
 import { Key } from 'react';
-import request from '../common/request';
+import { fetch } from '../common/request';
 
 export interface StaffResponse {
   customerName: string;
@@ -15,10 +15,34 @@ export interface StaffResponse {
   logoUrl?: string;
 }
 
-export const getStaffList = (muid: string) => request<StaffResponse[]>({ url: `/api/staff/list?muid=${muid}` });
+export interface StaffResponse {
+  customerName: string;
+  customerFullName: string;
+  parentCustomerName: string;
+  parentCustomerFullName: string;
+  id: number;
+  muid: string;
+  staffCode: null | string;
+  tenantName: string;
+  customerMcid: string;
+  tenantMcid: string;
+  logoUrl?: string;
+}
 
-export const checkStaff = (id: Key) => request<any>({ url: `/api/staff/check?id=${id}` });
+export function getStaffList(muid: string): Promise<StaffResponse[]> {
+  return fetch(`/api/staff/list?muid=${muid}`);
+}
 
-export const switchStaff = (id: Key) => {
-  request<void>({ url: `/api/staff/switch?staff_id=${id}`, method: 'PUT' });
-};
+export function checkStaff(id: Key): Promise<any> {
+  return fetch(`/api/staff/check?id=${id}`);
+}
+
+export function switchStaff(id: Key): Promise<void> {
+  return fetch(`/api/staff/switch?staff_id=${id}`, {
+    method: 'PUT',
+  });
+}
+
+export function getStaffPermission(): Promise<string[]> {
+  return fetch(`/api/staff/permission`);
+}
--- packages/utils/src/common/request.ts
+++ packages/utils/src/common/request.ts
@@ -1,58 +1,133 @@
-import axios, { AxiosRequestConfig } from 'axios';
-import { message } from 'antd';
-import { Subject } from 'rxjs';
+import { fetch as request, ResponseCodeError } from './fetch';
+import { Modal } from 'antd';
 
-const httpStatusSubject = new Subject<number>();
-
-declare module 'axios' {
-  // noinspection JSUnusedGlobalSymbols
-  export interface AxiosRequestConfig {
-    onError?: (err: any) => void;
+async function fetch<T>(input: RequestInfo, init?: RequestInit): Promise<T> {
+  try {
+    return await request<T>(input, init);
+  } catch (e) {
+    if (e instanceof ResponseCodeError) {
+      throw new ResponseError(e);
+    } else {
+      throw e;
+    }
   }
 }
 
-const instance = axios.create({
-  withCredentials: true,
-});
+interface ErrorHandler {
+  (err: any): boolean;
+}
 
-const defaultErrorHandler = (err: any) => {
-  const defaultMessage = err?.response?.data?.errorMessage || '未知错误！ 请联系管理员。';
-  switch (err.response?.status) {
-    case 400:
-      message.error(defaultMessage);
-      break;
-    case 409:
-      console.error(defaultMessage);
-      break;
-    case 410:
-      console.error(defaultMessage);
-      break;
-    case 412:
-      if (!/^\/select-staff/.test(window.location.pathname)) {
-        window.location.assign(
-          (window.SELECTSTAFFORIGIN ?? '') + '/select-staff?redirect=' + encodeURIComponent(window.location.href)
-        );
-      }
-      return true;
-    case 500:
-      message.error(defaultMessage);
-      break;
+export type PromiseWithErrorHandler<T> = Promise<T> & ErrorHandlerReceiver<T>;
+
+interface ErrorHandlerReceiver<T> {
+  onError(handler: ErrorHandler): PromiseWithErrorHandler<T>;
+  then<TResult1 = T, TResult2 = never>(
+    onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null,
+    onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null
+  ): Promise<TResult1 | TResult2>;
+}
+
+export function defaultErrorHandling(err: any): boolean {
+  let rce: ResponseCodeError | undefined;
+  let defaultMessage = '未知错误！ 请联系管理员。';
+
+  if (err instanceof ResponseCodeError) {
+    rce = err;
+  } else if (err instanceof ResponseError) {
+    rce = err.cause;
+    defaultMessage = err.message || err.cause.errorMessage || defaultMessage;
   }
+  if (rce) {
+    switch (rce.status) {
+      case 400:
+        Modal.error({
+          title: defaultMessage,
+        });
+        return true;
+      case 500:
+        Modal.error({
+          title: defaultMessage,
+        });
+        return true;
+      case 412:
+        if (!/^\/select-staff/.test(window.location.pathname)) {
+          window.location.assign(
+            (window.SELECTSTAFFORIGIN ?? '') + '/select-staff?redirect=' + encodeURIComponent(window.location.href)
+          );
+        }
+        return true;
+    }
+  }
+  return false;
+}
+
+export class ResponseError extends Error {
+  readonly cause: ResponseCodeError;
+  readonly error?: string;
+  readonly errorCode?: string;
+  readonly message: string;
+  readonly path?: string;
+  readonly status?: string;
+  readonly timestamp?: string;
+
+  constructor(cause: ResponseCodeError) {
+    super();
+    this.cause = cause;
+    this.error = cause?.body?.error;
+    this.errorCode = cause?.body?.error_code;
+    this.message = cause?.body?.message;
+    this.path = cause?.body?.path;
+    this.status = cause?.body?.status;
+    this.timestamp = cause?.body?.timestamp;
+  }
+}
+
+interface DefaultErrorHandler {
+  (err: any): boolean;
+}
+
+interface fetchConfigProps {
+  handler: DefaultErrorHandler;
+}
+
+export const createFetchInstance = (defaultConfig?: fetchConfigProps) => {
+  const { handler = defaultErrorHandling } = defaultConfig ?? {};
+
+  return function fetchWithDefaultErrorHandling<T>(input: RequestInfo, init?: RequestInit): PromiseWithErrorHandler<T> {
+    let errorHandler: ErrorHandler | undefined;
+    let callThen = true;
+
+    const result = fetch(input, init).catch(e => {
+      if (errorHandler) {
+        callThen = !errorHandler(e);
+      } else {
+        callThen = !handler(e);
+      }
+      if (callThen) {
+        throw e;
+      }
+    });
+    const mergedResult: PromiseWithErrorHandler<T> = result as PromiseWithErrorHandler<T>;
+
+    mergedResult.onError = (handler: ErrorHandler) => {
+      errorHandler = handler;
+      return mergedResult;
+    };
+
+    const _then = mergedResult.then;
+    mergedResult.then = <T, nerver>(onfulfilled: any, onrejected: any) => {
+      const fulfilled = (...args: any[]) => {
+        if (callThen) {
+          return onfulfilled(...args);
+        }
+      };
+      return _then.apply(mergedResult, [fulfilled, onrejected]) as Promise<T>;
+    };
+
+    return mergedResult;
+  };
 };
 
-instance.interceptors.response.use(
-  res => {
-    httpStatusSubject.next(res.status);
-    return res.data;
-  },
-  err => {
-    httpStatusSubject.next(err.response?.status);
-    const errorHandler = err.config?.onError || defaultErrorHandler;
-    errorHandler(err);
-    return Promise.reject(err);
-  }
-);
+const fetchInstance = /*#__PURE__*/ createFetchInstance();
 
-const request = <T>(cfg: AxiosRequestConfig) => instance.request<nerver, T>(cfg);
-
-export default request;
+export { fetchInstance as fetch };
