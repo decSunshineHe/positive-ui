6bcef5a36cdee430e542424cf2921455145f9f0c
decSunshineHe <1136031918@qq.com>
2023-05-29 16:27:48 +0800

    feat: 测试发版

--- packages/components/src/MessageCenter/index.tsx
+++ packages/components/src/MessageCenter/index.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react';
 const MessageCenter: FC = () => {
   return (
     <div>
-      <h1>SwitchStaff测试</h1>
+      <h1>SwitchStaff测试2</h1>
     </div>
   );
 };
--- packages/utils/src/common/hooks.ts
+++ packages/utils/src/common/hooks.ts
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export function useCurrent() {
   useEffect(() => {
-    console.log('测试一下demo');
+    console.log('测试一下demo2');
   }, []);
 
   return '小何的测试用户';
